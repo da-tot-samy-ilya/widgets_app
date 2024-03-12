bb6315fb21c2d07661fca088e810c4d5a2ad37c8
Ilya <bannikov_iv@sima-land.ru>
2024-03-12 18:15:25 +0500

    Поправил ссылку

--- src/widgets/weather/api/api.ts
+++ src/widgets/weather/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://api.weatherapi.com/v1/";
+const BASE_URL = "https://api.weatherapi.com/v1/";
 
 export const api = axios.create({
   baseURL: BASE_URL,
